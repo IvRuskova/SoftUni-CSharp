16d58bc62edf6bb26aaedfb8118be696dea0e784
Iva <IvRuskova@gmail.com>
2024-06-14 18:11:07 +0300

    Add excersices

---
+++ FundC#01'23/06.ObjectsAndClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _06.ObjectsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            Random rnd = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int newIndex = rnd.Next(0, words.Length);
+                string currWord = words[i];
+                words[i] = words[newIndex];
+                words[newIndex] =currWord;
+            }
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
