68aa3fe9c646420bc2fe205401b2962f710b2587
Iva <IvRuskova@gmail.com>
2024-06-17 17:03:35 +0300

    Add file 06. Classes and Objects

---
+++ FundC#01'23/01.AdvertisementMessage/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int numberOfMessages = int.Parse(Console.ReadLine());
+Random rand =new Random();
+List<string> phrases = new List<string>()
+{
+    "Excellent product.",
+    "Such a great product.",
+    "I always use that product.",
+    "Best product of its category.",
+    "Exceptional product.",
+    "I can’t live without this product."
+};
+List<string> events = new List<string>() 
+{
+    "Now I feel good.",
+    "I have succeeded with this product.",
+    "Makes miracles. I am happy of the results!",
+    "I cannot believe but now I feel awesome.",
+    "Try it yourself, I am very satisfied.",
+    "I feel great!"
+};
+List<string> authors = new List<string>()
+{
+    "Diana",
+    "Petya",
+    "Stella",
+    "Elena",
+    "Katya",
+    "Iva",
+    "Annie",
+    "Eva"
+};
+List<string> cities = new List<string>()
+{
+    "Burgas",
+    "Sofia",
+    "Plovdiv",
+    "Varna",
+    "Ruse"
+};
+for (int i = 1; i <= numberOfMessages; i++)
+{
+    int phraseIndex = rand.Next(phrases.Count);
+    int eventIndex = rand.Next(events.Count);
+    int authorIndex = rand.Next(authors.Count);
+    int cityIndex = rand.Next(cities.Count);
+    Console.WriteLine($"{phrases[phraseIndex]} {events[eventIndex]} {authors[authorIndex]} - {cities[cityIndex]}");
+}
---
+++ FundC#01'23/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.StoreBoxes
+{
+    internal class Box
+    {
+        public Box(int serialNumber, int itemQuantity, decimal priceBox,Item item) 
+        {
+            SerialNumber = serialNumber;
+            ItemQuantity = itemQuantity;
+            PriceBox = priceBox;
+            Item =  item;
+        }
+        public int SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceBox { get; set; }
+    }
+}
