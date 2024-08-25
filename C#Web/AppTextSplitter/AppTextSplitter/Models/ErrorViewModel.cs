b661446a1399fc65c140ac651797dbea9c8c857a
Iva <IvRuskova@gmail.com>
2024-08-26 01:10:25 +0300

    Add AppTextSplitter

---
+++ C#Web/AppTextSplitter/AppTextSplitter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppTextSplitter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ C#Web/AppTextSplitter/AppTextSplitter/Models/TextViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AppTextSplitter.Models
+{
+    public class TextViewModel
+    {
+        /// <summary>
+        /// This is the text
+        /// </summary>
+        public string Text { get; set; } = null!;
+
+        /// <summary>
+        /// This is the SplitText
+        /// </summary>
+        public string SplitText { get; set; } = null!;
+    }
+}
