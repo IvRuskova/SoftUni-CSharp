8e6e767fe1e4f2466dfba09b932b1aeef1d67ac4
Iva <IvRuskova@gmail.com>
2024-08-25 21:34:15 +0300

    Add SimpleAppChat

---
+++ C#Web/SimpleChatApp/SimpleChatApp/Controllers/ChatController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleChatApp.Models.Message;
+
+
+namespace SimpleChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string,string>> s_messages =new();
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            if (s_messages.Count() < 1)
+            {
+                return View(new ChatViewModel());
+            }
+            var chatModel = new ChatViewModel()
+            {
+                Messages = s_messages
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessagesText = m.Value
+                })
+                .ToList()
+            };
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessage = chat.CurrentMessage;
+            s_messages.Add(new KeyValuePair<string, string>
+                (newMessage.Sender, newMessage.MessagesText));
+            return RedirectToAction("Show");
+        }
+    }
+}
