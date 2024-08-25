a84813e32269145ae53ab763ca3e327f3771cd2b
Iva <IvRuskova@gmail.com>
2024-08-26 01:14:38 +0300

    Add more excersices

--- C#Web/FormApp/FormApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FormApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
